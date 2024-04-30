a3f5b3aded1f18dd257b900e8c473870c15035cd
shahinexy <shahinexy@gmail.com>
2024-04-30 22:53:13 +0600

    some issue solved

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+    return (
+        <div>
+         <div className="w-full h-screen flex justify-center items-center text-forth"><span className="loading loading-infinity loading-lg"></span></div>
+        </div>
+    );
+};
+
+export default Loader;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,28 +5,46 @@ import ExtraRoute1 from "../components/ExtraRoute1";
 import ExtraSection2 from "../components/ExtraSection2";
 import CategorySection from "../components/CategorySection";
 import { Helmet } from "react-helmet";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
 // import Testing from "../components/Testing";
 
 const Home = () => {
   const allItems = useLoaderData();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (allItems) {
+      setLoading(false);
+    }
+  }, [allItems]);
+
   return (
     <div className="overflow-hidden">
       <Helmet>
         <title>ShinArt | Home</title>
       </Helmet>
-      <Slider></Slider>
 
-      <div className="max-w-7xl mx-auto px-5">
-        <CraftItems allItems={allItems}></CraftItems>
-      </div>
+      {loading ? (
+        <Loader></Loader>
+      ) : (
+        <>
+          <Slider></Slider>
 
-      <ExtraRoute1></ExtraRoute1>
+          <div className="max-w-7xl mx-auto px-5">
+            <CraftItems allItems={allItems}></CraftItems>
+          </div>
 
-      <div className="max-w-7xl mx-auto px-5">
-        <CategorySection></CategorySection>
-      </div>
+          <ExtraRoute1></ExtraRoute1>
 
-      <ExtraSection2></ExtraSection2>
+          <div className="max-w-7xl mx-auto px-5">
+            <CategorySection></CategorySection>
+          </div>
+
+          <ExtraSection2></ExtraSection2>
+        </>
+      )}
     </div>
   );
 };
